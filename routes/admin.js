b6783f8e707b05de154ad0bf4d98428a6abf897c
Charlie <chen_610146@hotmail.com>
2019-07-26 22:19:02 +0800

    Admin product page

--- routes/admin.js
+++ routes/admin.js
@@ -5,4 +5,8 @@ router.get('/customers', (req, res) => {
   res.render('admin/customers')
 })
 
+router.get('/products', (req, res) => {
+  res.render('admin/products')
+})
+
 module.exports = router
