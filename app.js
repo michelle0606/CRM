9081177f427c6b47814a459aef6cda91619ce1dc
Charlie <chen_610146@hotmail.com>
2019-07-26 22:11:47 +0800

    Admin customer page

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path')
 const logger = require('morgan')
 const indexRouter = require('./routes/index')
 const usersRouter = require('./routes/users')
+const adminRouter = require('./routes/admin')
 
 const app = express()
 
@@ -18,14 +19,15 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', indexRouter)
 app.use('/users', usersRouter)
+app.use('/admin', adminRouter)
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404))
 })
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message
   res.locals.error = req.app.get('env') === 'development' ? err : {}
@@ -33,6 +35,10 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500)
   res.render('error')
+})
+
+app.listen(3000, () => {
+  console.log('Running')
 })
 
 module.exports = app
