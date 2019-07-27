0e1f5f24019c25525d56d3527fefabc5e329ccc9
lihsu <lihsu3@gmail.com>
2019-07-27 09:47:55 +0800

    li - signup and login

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const path = require('path')
 const logger = require('morgan')
 const indexRouter = require('./routes/index')
 const usersRouter = require('./routes/users')
+const passport = require('./config/passport')
+const flash = require('connect-flash')
+const session = require('express-session')
 
 const app = express()
 
@@ -15,6 +18,10 @@ app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
+
+app.use(passport.initialize())
+app.use(passport.session())
 
 app.use('/', indexRouter)
 app.use('/users', usersRouter)
--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,20 @@
 const express = require('express')
 const router = express.Router()
+const userController = require('../controllers/userController')
+const passport = require('../config/passport')
 
 /* GET home page. */
 router.get('/', (req, res) => {
+	// console.log(req.user)
   res.render('index', { title: 'Waromen' })
 })
 
-router.get('/login', (req, res) => {
-  res.render('login', { title: '登入' })
-})
+router.get('/login', userController.signInPage)
+router.post('/login', passport.authenticate('local', { failureRedirect: '/login', failureFlash: true }), userController.signIn)
+router.get('/logout', userController.logout)
 
-router.get('/signup', (req, res) => {
-  res.render('signup', { title: '開啟服務' })
-})
+router.get('/signup', userController.signUpPage)
+router.post('/signup', userController.signUp)
 
 router.get('/shop/:shop_id', (req, res) => {
   res.render('index', { title: 'Waromen' })
