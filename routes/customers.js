dc5c5eebbf404a91d59d5bfa96b0503989a17261
walle <michelle210181@gmail.com>
2019-07-29 18:07:29 +0800

    solve the bug in tradeController.js and customerController.js, also change some input's type from text to number

--- routes/customers.js
+++ routes/customers.js
@@ -2,7 +2,6 @@ const express = require('express')
 const router = express.Router()
 const customerController = require('../controllers/customerController')
 const tradeControlloer = require('../controllers/tradeController')
-const passport = require('../config/passport')
 
 const authenticated = (req, res, next) => {
   if (req.isAuthenticated()) {
@@ -15,7 +14,15 @@ router.get('/', authenticated, customerController.getAllCustomers)
 router.post('/', authenticated, customerController.addCustomer)
 
 router.get('/:customers_id', authenticated, customerController.getCustomer)
-router.get('/:customers_id/record', tradeControlloer.getCustomerTradePage)
-router.post('/:customers_id/record', tradeControlloer.createNewTradeRecord)
+router.get(
+  '/:customers_id/record',
+  authenticated,
+  tradeControlloer.getCustomerTradePage
+)
+router.post(
+  '/:customers_id/record',
+  authenticated,
+  tradeControlloer.createNewTradeRecord
+)
 
 module.exports = router
