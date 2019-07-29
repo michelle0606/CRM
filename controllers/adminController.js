9018e6670f7067ee83dc456491c5a07688d695c7
lihsu <lihsu3@gmail.com>
2019-07-29 23:16:17 +0800

    li - backstage user CRUD

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,11 +1,11 @@
 const db = require('../models')
-// const User = db.User
+const User = db.User
 const Shop = db.Shop
 const imgur = require('imgur-node-api')
 
 const adminController = {
 	createShop: (req, res) => {
-		return res.render('advance/create')
+		return res.render('advance/createShop')
 	},
 
 	postShop: (req, res) => {
@@ -52,7 +52,7 @@ const adminController = {
 
 	getShop: (req, res) => {
 		return Shop
-		  .findByPk(req.params.id)
+		  .findByPk(req.params.shop_id)
 		  .then(shop => {
 		  	res.render('advance/shop', { shop })
 		  })
@@ -60,9 +60,9 @@ const adminController = {
 
   editShop: (req, res) => {
 		return Shop
-			.findByPk(req.params.id)
+			.findByPk(req.params.shop_id)
 			.then(shop => {
-				return res.render('advance/create', { shop })
+				return res.render('advance/createShop', { shop })
    		})
   },
 
@@ -77,7 +77,7 @@ const adminController = {
       imgur.setClientID(process.env.IMGUR_CLIENT_ID);
       imgur.upload(file.path, (err, img) => {
         Shop
-          .findByPk(req.params.id)
+          .findByPk(req.params.shop_id)
           .then((shop) => {
             shop
               .update({
@@ -90,13 +90,13 @@ const adminController = {
               .then(() => {
                 req.flash('success_messages', '修改成功');
                 res.redirect('/advance/shops')
-                // res.redirect(`/advance/shops/${req.params.id}`);
+                // res.redirect(`/advance/shops/${req.params.shop_id}`);
               });
           });
       });
     } else {
       Shop
-        .findByPk(req.params.id)
+        .findByPk(req.params.shop_id)
         .then((shop) => {
           shop
             .update({
@@ -109,7 +109,7 @@ const adminController = {
             .then(() => {
               req.flash('success_messages', '修改成功');
               res.redirect('/advance/shops')
-              // res.redirect(`/advance/shops/${req.params.id}`);
+              // res.redirect(`/advance/shops/${req.params.shop_id}`);
             });
         });
     }
@@ -119,11 +119,130 @@ const adminController = {
   	return Shop
   		.destroy({ 
   			where: {
-  				id: Number(req.params.id) 
+  				id: Number(req.params.shop_id) 
   			}
   		})
       .then(() => {
         return res.redirect('/advance/shops')
+      })
+  },
+
+  createUser: (req, res) => {
+    return res.render('advance/createUser')
+  },
+
+  postUser: (req, res) => {
+    if (!req.body.name) {
+      req.flash('error_messages', '需要有名字')
+      return res.redirect('back')
+    }
+    const { file } = req // equal to const file = req.file
+    if (file) {
+      imgur.setClientID(process.env.IMGUR_CLIENT_ID)
+      imgur.upload(file.path, (err, img) => {
+        return User.create({
+          name: req.body.name,
+          role: req.body.role,
+          ShopId: req.body.ShopId,
+          avatar: file ? img.data.link : null,
+        }).then((user) => {
+          req.flash('success_messages', '新增成功')
+          res.redirect('/advance/users')
+        })
+      })
+    } else {
+      return User.create({
+        name: req.body.name,
+        role: req.body.role,
+        ShopId: req.body.ShopId,
+      })
+      .then((user) => {
+        req.flash('success_messages', '新增成功')
+        res.redirect('/advance/users')
+      })
+    }
+  },
+
+  getUsers: (req, res) => {
+    return User
+      .findAll()
+      .then(users => {
+        res.render('advance/users', { users })
+      })
+  },
+
+  getUser: (req, res) => {
+    return User
+      .findByPk(req.params.user_id)
+      .then(user => {
+        res.render('advance/user', { user })
+      })
+  },
+
+  editUser: (req, res) => {
+    return User
+      .findByPk(req.params.user_id)
+      .then(user => {
+        return res.render('advance/createUser', { user })
+      })
+  },
+
+  putUser: (req, res) => {
+    if (!req.body.name) {
+      req.flash('error_messages', '需要有名字');
+      return res.redirect('back');
+    }
+
+    const { file } = req
+    if (file) {
+      imgur.setClientID(process.env.IMGUR_CLIENT_ID);
+      imgur.upload(file.path, (err, img) => {
+        User
+          .findByPk(req.params.user_id)
+          .then((user) => {
+            user
+              .update({
+                name: req.body.name,
+                role: req.body.role,
+                ShopId: req.body.ShopId,
+                avatar: file ? img.data.link : user.avatar,
+              })
+              .then(() => {
+                req.flash('success_messages', '修改成功');
+                res.redirect('/advance/users')
+                // res.redirect(`/advance/users/${req.params.user_id}`);
+              });
+          });
+      });
+    } else {
+      User
+        .findByPk(req.params.user_id)
+        .then((user) => {
+          user
+            .update({
+              name: req.body.name,
+              role: req.body.role,
+              ShopId: req.body.ShopId,
+              avatar: user.avatar,
+            })
+            .then(() => {
+              req.flash('success_messages', '修改成功');
+              res.redirect('/advance/users')
+              // res.redirect(`/advance/users/${req.params.user_id}`);
+            });
+        });
+    }
+  },
+
+  deleteUser: (req, res) => {
+    return User
+      .destroy({ 
+        where: {
+          id: Number(req.params.user_id) 
+        }
+      })
+      .then(() => {
+        return res.redirect('/advance/users')
       })
   },
 }
--- routes/advance.js
+++ routes/advance.js
@@ -25,16 +25,28 @@ router.get('/sales', (req, res) => {
   res.render('advance/sales')
 })
 
-// create
+// shop create
 router.get('/shops/create', authenticatedAdmin, adminController.createShop)
 router.post('/shops', authenticatedAdmin, upload.single('logo'), adminController.postShop)
-// read
+// shop read
 router.get('/shops', authenticatedAdmin, adminController.getShops)
-router.get('/shops/:id', authenticatedAdmin, adminController.getShop)
-// update
-router.get('/shops/:id/edit', authenticatedAdmin, adminController.editShop)
-router.put('/shops/:id', authenticatedAdmin, upload.single('logo'), adminController.putShop)
-// delete
-router.delete('/shops/:id', authenticatedAdmin, adminController.deleteShop)
+router.get('/shops/:shop_id', authenticatedAdmin, adminController.getShop)
+// shop update
+router.get('/shops/:shop_id/edit', authenticatedAdmin, adminController.editShop)
+router.put('/shops/:shop_id', authenticatedAdmin, upload.single('logo'), adminController.putShop)
+// shop delete
+router.delete('/shops/:shop_id', authenticatedAdmin, adminController.deleteShop)
+
+// user create
+router.get('/users/create', authenticatedAdmin, adminController.createUser)
+router.post('/users', authenticatedAdmin, upload.single('avatar'), adminController.postUser)
+// user read
+router.get('/users', authenticatedAdmin, adminController.getUsers)
+router.get('/users/:user_id', authenticatedAdmin, adminController.getUser)
+// user update
+router.get('/users/:user_id/edit', authenticatedAdmin, adminController.editUser)
+router.put('/users/:user_id', authenticatedAdmin, upload.single('avatar'), adminController.putUser)
+// user delete
+router.delete('/users/:user_id', authenticatedAdmin, adminController.deleteUser)
 
 module.exports = router
