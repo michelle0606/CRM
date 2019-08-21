88a72baf398c4903994759d2d0777581b50db8a0
lihsu <lihsu3@gmail.com>
2019-08-22 00:21:29 +0800

    li - user model unit test; add id; edit scripts

--- tests/models/user.test.js
+++ tests/models/user.test.js
@@ -1,8 +1,49 @@
+var chai = require('chai')
+// var sinon = require('sinon')
+chai.use(require('sinon-chai'))
 const { expect } = require('chai')
+const {
+  sequelize,
+  dataTypes,
+  checkModelName,
+  checkUniqueIndex,
+  checkPropertyExists
+} = require('sequelize-test-helpers')
 const db = require('../../models')
+const UserModel = require('../../models/user')
 
 describe('# User Model', () => {
-  describe('CRUD', () => {
+  const User = UserModel(sequelize, dataTypes)
+  const user = new User()
+
+  context('check model name', () => {
+    checkModelName(User)('User')
+  })
+
+  context('check properties', () => {
+    ;[
+      'name', 'password', 'role'
+    ].forEach(checkPropertyExists(user))
+  })
+
+  context('check associations', () => {
+    const Sale = ''
+    const Shop = ''
+
+    before(() => {
+      User.associate({ Sale })
+      User.associate({ Shop })
+    })
+
+    it('defined a hasMany association with Sale', () => {
+      expect(User.hasMany).to.have.been.calledWith(Sale)
+    })
+    it('defined a belongsTo association with Shop', () => {
+      expect(User.belongsTo).to.have.been.calledWith(Shop)
+    })
+  })
+
+  context('check CRUD', () => {
     let data = null
 
     it('create', done => {
