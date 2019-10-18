3ccccc92b12ae71858e182ca0d38f77ddd928954
walle <michelle210181@gmail.com>
2019-10-18 21:35:48 +0800

    fix error when upload new product

--- controllers/productController.js
+++ controllers/productController.js
@@ -13,57 +13,65 @@ const productController = {
   },
 
   postInventory: async (req, res) => {
-    let productsFile = req.files.products
-    let uploadPath = '../uploadCsv' + productsFile.name
+    let regex = /\.(csv)$/i
+    if (!req.files) {
+      req.flash('top_messages', '請選擇檔案！')
+      return res.redirect('/inventory')
+    } else if (!regex.test(req.files.products.name)) {
+      req.flash('top_messages', '檔案格式只接受CSV！')
+      return res.redirect('/inventory')
+    } else {
+      let productsFile = req.files.products
+      let uploadPath = '../uploadCsv' + productsFile.name
 
-    // 先把上傳的csv檔案存到指定資料夾
-    productsFile.mv(uploadPath, function(err) {
-      if (err) {
-        return res.status(500).send(err)
-      }
-    })
-
-    // 到指定資料夾讀取csv檔，轉換成json格式
-    const jsonArrayObj = await csv().fromFile(uploadPath)
-
-    // 把json格式的進貨資料，儲存到資料庫
-    const record = await PurchaseRecord.create({
-      UserId: req.user.id,
-      ShopId: req.user.ShopId
-    })
-
-    jsonArrayObj.forEach(async el => {
-      const data = await PurchaseRecordDetail.create({
-        quantity: el.quantity,
-        ProductId: Number(el.ProductId),
-        PurchaseRecordId: record.id
+      // 先把上傳的csv檔案存到指定資料夾
+      productsFile.mv(uploadPath, function(err) {
+        if (err) {
+          return res.status(500).send(err)
+        }
       })
 
-      const existProduct = await Product.findByPk(data.ProductId, {
-        where: { ShopId: req.user.ShopId }
+      // 到指定資料夾讀取csv檔，轉換成json格式
+      const jsonArrayObj = await csv().fromFile(uploadPath)
+      // 把json格式的進貨資料，儲存到資料庫
+      const record = await PurchaseRecord.create({
+        UserId: req.user.id,
+        ShopId: req.user.ShopId
       })
-      if (existProduct) {
-        // 現有產品更新資訊
-        const newInventory =
-          Number(existProduct.inventory) + Number(data.quantity)
-        existProduct.update({
-          name: el.name,
-          salePrice: el.salePrice,
-          inventory: newInventory
-        })
-      } else {
-        // 新產品
-        Product.create({
-          id: el.ProductId,
-          name: el.name,
-          salePrice: el.salePrice,
-          ShopId: req.user.ShopId,
-          inventory: el.quantity
-        })
-      }
-    })
 
-    res.redirect('/inventory')
+      // 匯入的庫存資料不具備ShopId，必須先判斷產品編號是否已經存在，並且確認該產品的ShopId，如果存在，
+      jsonArrayObj.forEach(async el => {
+        const data = await PurchaseRecordDetail.create({
+          quantity: el.quantity,
+          ProductId: Number(el.ProductId),
+          PurchaseRecordId: record.id
+        })
+
+        const existProduct = await Product.findOne({
+          where: { id: Number(data.ProductId), ShopId: req.user.ShopId }
+        })
+        if (existProduct) {
+          // 現有產品更新資訊
+          const newInventory =
+            Number(existProduct.inventory) + Number(data.quantity)
+          existProduct.update({
+            name: el.name,
+            salePrice: el.salePrice,
+            inventory: newInventory
+          })
+        } else {
+          // 新產品
+          Product.create({
+            name: el.name,
+            salePrice: el.salePrice,
+            ShopId: req.user.ShopId,
+            inventory: el.quantity
+          })
+        }
+      })
+      req.flash('top_messages', '成功上傳檔案！')
+      return res.redirect('/inventory')
+    }
   },
 
   getPurchaseRecords: (req, res) => {
